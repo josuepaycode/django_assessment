95b7cf0b76b55adc39a5bb6808ce0aaa2e88a1b3
Joselo <jl.garciareymundo@gmail.com>
2023-03-09 23:04:51 -0600

    custom UserAdmin

--- django_test/authentication/admin.py
+++ django_test/authentication/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Administrator
+from .models import Administrator, UserAdmin
 
-# Register your models here.
-admin.site.register(Administrator)
+
+admin.site.register(Administrator, UserAdmin)
--- django_test/authentication/models.py
+++ django_test/authentication/models.py
@@ -1,6 +1,7 @@
 import jwt
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
@@ -46,6 +47,7 @@ class CustomUserManager(UserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self._create_user(username, email, password, **extra_fields)
+
 
 class Administrator(AbstractBaseUser, PermissionsMixin):
     """
@@ -113,3 +115,18 @@ class Administrator(AbstractBaseUser, PermissionsMixin):
             algorithm='HS256',
         )
         return token
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email']
+    readonly_fields = ('date_joined','last_login')
+    fieldsets = (
+        (_("User Details"), {'fields': ('email', 'password')}),
+        (_("Account Details"), {'fields': ('date_joined', 'last_login')}),
+        (_("Permission"), {'fields': ('is_active', 'is_staff', 'is_admin')}),
+    )
+    add_fieldsets = (
+        ("User Details", {'fields': ('email', 'password1', 'password2')}),
+        ("Permission", {'fields': ('is_active', 'is_staff', 'is_admin')}),
+    )
