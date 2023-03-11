559721f2c7507dda74bdde764c50560893419fbe
Joselo <jl.garciareymundo@gmail.com>
2023-03-10 19:12:07 -0600

    added swagger doc

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -15,9 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="PayCode API",
+        default_version='v1',
+        description="Documentation for Challenge",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="jl.garciareymundo@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('webApp.urls')),
     path('api/auth/', include('authentication.urls')),
+    path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
